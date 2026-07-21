d43ba17c64c77a89a2223da8ad3c9a472df63f60
Тимофей Приходько <t232006@gmail.com>
2026-07-21 14:18:13 +0200

    swap compl key

--- Migrations/20260721122352_migr1.Designer.cs
+++ Migrations/20260721122352_migr1.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SyncServer;
@@ -11,9 +12,11 @@ using SyncServer;
 namespace SyncServer.Migrations
 {
     [DbContext(typeof(SyncDBContext))]
-    partial class SyncDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260721122352_migr1")]
+    partial class migr1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260721122352_migr1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class migr1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_topic",
+                table: "topic");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_dict",
+                table: "dict");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_topic",
+                table: "topic",
+                columns: new[] { "DBID", "Modification_Time" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_dict",
+                table: "dict",
+                columns: new[] { "DBID", "Modification_Time" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_topic",
+                table: "topic");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_dict",
+                table: "dict");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_topic",
+                table: "topic",
+                columns: new[] { "Modification_Time", "DBID" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_dict",
+                table: "dict",
+                columns: new[] { "Modification_Time", "DBID" });
+        }
+    }
+}
